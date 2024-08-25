8ec4851b829e27a960debf7e51f45fffd9884ac5
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-08-25 17:47:00 +0530

    room adding logic added

--- components/docActions/AddDocBtn.tsx
+++ components/docActions/AddDocBtn.tsx
@@ -2,9 +2,27 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import Image from 'next/image'
+import { createDocument } from '@/lib/actions/room.actions'
+import { useRouter } from 'next/navigation'
 
 const AddDocBtn = ({userId, email} :AddDocumentBtnProps ) => {
+  const router = useRouter();
   const addDocumentHandler = async () =>{
+    try {
+     const room = await createDocument({userId, email})
+
+     if(room) {
+       console.log('Room created', room)
+     }
+     if(!room) {
+       console.log('Room not created')
+     }
+     if(room) router.push(`/documents/${room.id}`)
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
     
   }
 
