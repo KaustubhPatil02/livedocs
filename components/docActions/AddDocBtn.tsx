6ed9c0e975be645249e6a68bf75e867d8f2c0a7f
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-08-30 21:01:17 +0530

    feat: Improve AddDocBtn component with confetti animation and loading state

--- components/docActions/AddDocBtn.tsx
+++ components/docActions/AddDocBtn.tsx
@@ -1,40 +1,54 @@
 'use client'
-import React from 'react'
-import { Button } from '../ui/button'
-import Image from 'next/image'
-import { createDocument } from '@/lib/actions/room.actions'
-import { useRouter } from 'next/navigation'
+import React, { useState } from 'react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import { createDocument } from '@/lib/actions/room.actions';
+import { useRouter } from 'next/navigation';
+import { ConfettiButton } from "@/components/magicui/confetti";
+import Loader from '../Loader';
 
 const AddDocBtn = ({ userId, email }: AddDocumentBtnProps) => {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
+
   const addDocumentHandler = async () => {
+    setLoading(true);
     try {
-      const room = await createDocument({ userId, email })
-      if (room) router.push(`/documents/${room.id}`)
-
+      const room = await createDocument({ userId, email });
+      if (room) router.push(`/documents/${room.id}`);
     } catch (error) {
-      console.log(error)
-
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-
-  }
-
+  };
 
   return (
-    <Button type='submit' onClick={addDocumentHandler}
-      className='gradient-slate flex gap-1 shadow-md'
-    >
-      <Image
-        src='/assets/icons/add.svg'
-        alt='Add document icon'
-        width={20}
-        height={20}
-      />
-      <p className='hidden sm:block'>
-        Create a blank document!
-      </p>
-    </Button>
-  )
-}
+    <ConfettiButton className='bg-transparent'>
+      <Button
+        type='submit'
+        onClick={addDocumentHandler}
+        className={`gradient-green flex gap-1 shadow-md ${loading ? 'opacity-50' : ''}`}
+        disabled={loading}
+      >
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <Image
+              src='/assets/icons/add.svg'
+              alt='Add document icon'
+              width={20}
+              height={20}
+            />
+            <p className='hidden sm:block'>
+              Create a blank document!
+            </p>
+          </>
+        )}
+      </Button>
+    </ConfettiButton>
+  );
+};
 
-export default AddDocBtn
+export default AddDocBtn;
