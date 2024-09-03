6df35158f273f381d31d9cdad4a628f82aba2a2a
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-09-03 19:04:54 +0530

    chore: Update .gitignore and package.json, add Sentry configuration files

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e6f1bba226d3c935329ce269f50ccf43@o4507888433823744.ingest.de.sentry.io/4507888437100624",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
