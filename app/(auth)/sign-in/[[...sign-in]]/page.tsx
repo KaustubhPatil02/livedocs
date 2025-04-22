dbc1b6f4b9fc215d9924394e6eec6fc796322cc5
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-04-23 03:40:36 +0530

    fix: Correct HTML entity for apostrophe in SignInPage and adjust layout for consistency

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,11 @@
 import { AnimatedGridPattern } from '@/components/magicui/animated-grid-pattern'
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
 
 const SignInPage = () => {
   return (
-    <main className='auth-page'>
+    <main className='auth-page mt-4'>
       
       <AnimatedGridPattern
         className="absolute -z-10 opacity-30 fill-blue-900"
@@ -20,10 +21,21 @@ const SignInPage = () => {
           LiveDocs✨ is a live collaborative editor powered by LiveBlocks.
         </p>
         <p className="text-xl  text-blue-100 font-semibold">
-          Let`&apos;`s Log you in and start collaborating in real-time.
+          Let&apos;s Log you in and start collaborating in real-time.
         </p>
       </div>
     <SignIn />
+    {/* <div className='flex flex-col items-center justify-center gap-2'>
+            <p className='text-sm font-light text-blue-200'>Made with ❤️ by Kaustubh Patil</p>
+            <Image
+              src={'/kaustubh.png'}
+              width={60}
+              height={60}
+              alt="Kaustubh Patil"
+              className='rounded-full'
+            />
+            <a href="https://kaustubh02.vercel.app/"><p className='font-extralight font-sans'>➡️Learn more about the dev</p></a>
+          </div> */}
 </main>
   )
 }
